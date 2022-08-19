0e8eb021b5ba50d8e6d6a48078bd49f6e29cff39
Justin Cueva <justincueva@Justins-MBP.lan>
2022-08-18 21:50:47 -0700

    flipping the card after 3000ms

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from word_data import data
 
 words_to_learn = data.to_dict(orient="records")
 
-# TODO: 1. read the french words csv
-# TODO: 2. grap a random word and display it
-# TODO: 3. when clicking a button move to a new random card
+# TODO: 1. after 3 seconds flip the card
+# TODO: 2.
+# TODO: 3.
 
 flashy_ui = Flashy_UI(words_to_learn)
